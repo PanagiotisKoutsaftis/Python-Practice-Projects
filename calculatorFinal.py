e75ea5788c59050505d569140dcf0b9583ef41fc
PanagiotisKoutsaftis <83411830+PanagiotisKoutsaftis@users.noreply.github.com>
2021-07-14 09:32:42 +0300

    Update calculatorFinal.py

--- calculatorFinal.py
+++ calculatorFinal.py
@@ -3,8 +3,13 @@ import re
 def perform_math():
     global run
     global previous
+    equation = ""
+    if previous == 0:
+        equation = input("Enter equation: ")
+    else:
+        equation = input(str(previous))
     
-    equation = input("Enter the equation that you want to evaluate: ")
+    
     if equation == "quit":
         run = False
         print("Goodbye earthling")
@@ -14,15 +19,15 @@ def perform_math():
       
         if previous == 0:
             previous = eval(equation)
-            print(previous)
+            
         else:
             #making the previous variable a string so we can concatenate it with the equation string
             previous = str(previous)
             #concatenating the strings and then evaluating the whole equation
             previous = eval(previous + equation)
-            print(previous)
-        print("The equation you are going to type now, is going to be calculated considering the previous result, so start with an operation")
-        print("If you want to do a new calculation quit the program and start it again")
+           
+        
+        
 
 previous = 0
 run = True
